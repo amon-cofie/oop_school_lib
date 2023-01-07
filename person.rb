cef86dd2d6916422c9295d529dc2d98e209e22b8
David  Amon-Cofie <david@goyazi.com>
2023-01-07 04:16:04 -0800

    Add classes: Book, Classroom, Rental

--- person.rb
+++ person.rb
@@ -1,9 +1,10 @@
 require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'rental'
 
 class Person < Nameable
-  attr_accessor :name, :age, :parent_permissions
+  attr_accessor :name, :age, :parent_permissions, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -12,6 +13,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -20,6 +22,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
