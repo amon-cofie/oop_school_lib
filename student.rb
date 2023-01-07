4f6993a4efa4b2c1474d79045d490e941faf3e35
David  Amon-Cofie <david@goyazi.com>
2023-01-07 05:14:21 -0800

    Fix linter errors

--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(classroom, age, name, parent_permission)
     super(age, name, parent_permission)
     @classroom = classroom
